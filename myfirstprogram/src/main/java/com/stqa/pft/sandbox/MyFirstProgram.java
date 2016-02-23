082209b27a212d5abbf517a0291aff88cc2eb1a0
motoyarko <motoyarko@gmail.com>
2016-02-23 15:43:41 +0200

    2 functions added

--- myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,7 +4,27 @@ public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello, World");
+        hello("world!!!!");
+        hello("user");
+
+        double l = 7;
+        System.out.println("area 7 = " + area(l));
+        double a = 5;
+        double b = 4;
+        System.out.println("area 5 4 = " + area(a, b));
     }
 
+    public static void hello(String somebody) {
+        //String somebody = "world";
+
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double len) {
+        return len * len;
+    }
+
+    public static double area(double a, double b) {
+        return a * b;
+    }
 }

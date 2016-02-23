50971068dd4d80fcdfbef8c57d70de2291b74567
motoyarko <motoyarko@gmail.com>
2016-02-23 12:07:49 +0200

    created new project's structure

--- myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,4 +1,6 @@
-public class MyFirstProgram { 
+package com.stqa.pft.sandbox;
+
+public class MyFirstProgram {
    public static void main(String[] args) { 
       System.out.println("Hello, World");
    }

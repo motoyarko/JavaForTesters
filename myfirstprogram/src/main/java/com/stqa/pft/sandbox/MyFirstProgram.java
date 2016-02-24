79647d188795a60f21b9de8a36dbc1ecead8a5f4
motoyarko <motoyarko@gmail.com>
2016-02-24 13:02:53 +0200

    lesson 1 DZ

--- myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
+++ myfirstprogram/src/main/java/com/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,23 +1,31 @@
 package com.stqa.pft.sandbox;
 
+
+
 public class MyFirstProgram {
 
     public static void main(String[] args) {
 
-        hello("world!!!!");
+        Point p1 = new Point(1, 0);
+        Point p2 = new Point(3, 0);
+        Point point = new Point(1,1);
+        System.out.println("distance =" + point.distance(p1, p2));
+
+        /*hello("world!!!!");
         hello("user");
 
         Square s = new Square(7);
         System.out.println("area 7 = " + s.area());
 
         Rectangle r = new Rectangle(5, 4);
-        System.out.println("area 5 4 = " + r.area());
+        System.out.println("area 5 4 = " + r.area());*/
     }
 
     public static void hello(String somebody) {
 
         System.out.println("Hello, " + somebody + "!");
     }
+
 
 
 }
---
+++ myfirstprogram/src/main/java/com/stqa/pft/sandbox/Point.java
@@ -0,0 +1,18 @@
+package com.stqa.pft.sandbox;
+
+/**
+ * Created by antonenko on 2/24/2016.
+ */
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    protected double distance(Point p1, Point p2) {
+        return Math.sqrt((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y)) ;
+    }
+}
